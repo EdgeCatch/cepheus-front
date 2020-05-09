9f4464bfd302d7bf74ed4d9b071a71591ea38dda
Rexvel <teteriv22@gmail.com>
2020-05-09 19:07:25 +0300

    Redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, compose } from 'redux';
+import rootReducer from './reducers/index';
 
-const store = createStore(compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(
+    rootReducer,
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+);
 
 export default store;
