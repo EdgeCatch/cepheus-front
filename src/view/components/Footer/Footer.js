46f5e14af2a5fa1927496553df9c4b8c5ecff996
Rexvel <teteriv22@gmail.com>
2020-05-10 15:09:16 +0300

    Footer markup

--- src/view/components/Footer/Footer.js
+++ src/view/components/Footer/Footer.js
@@ -1,19 +1,36 @@
 // @flow
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 import './footer.scss';
 
 const Footer = () => {
     return (
-        <Navbar className="footer">
-            <Container className=" d-flex justify-content-around">
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-                <Col>2 of 2</Col>
-            </Container>
-        </Navbar>
+        <Container className="d-flex justify-content-between">
+            <Col className="d-flex flex-column">
+                <Link> Learn more</Link>
+                <Link>About Us </Link>
+                <Link>About TON</Link>
+                <Link>Terms And</Link>
+                <Link>Conditions</Link>
+                <Link>FAQ</Link>
+            </Col>
+            <Col className="d-flex flex-column">
+                <Link>Help</Link>
+                <Link>Subscriptions </Link>
+                <Link>Delivery</Link>
+                <Link>Withdraw And</Link>
+                <Link>Refund</Link>
+            </Col>
+            <Col className="d-flex flex-column">
+                <Link>Contact us </Link>
+                <Link>Telegram</Link>
+                <Link>Instagram</Link>
+                <Link>Facebook</Link>
+                <Link>Twitter</Link>
+            </Col>
+        </Container>
     );
 };
 
