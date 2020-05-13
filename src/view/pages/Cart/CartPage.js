d3ba4de4b4360eddadc8c2628f687fec6885e7b0
Rexvel <teteriv22@gmail.com>
2020-05-13 19:13:31 +0300

    PurchaseSummary block

--- src/view/pages/Cart/CartPage.js
+++ src/view/pages/Cart/CartPage.js
@@ -3,8 +3,18 @@ import Container from 'react-bootstrap/Container';
 import './CartPage.scss';
 
 const CartPage = () => (
-    <Container className="homePage">
-        <h2>What are you looking for?</h2>
+    <Container className="cartPage">
+        <h4 className="cartPage__headline">Cart</h4>
+        <div className="purchasesTotal">
+            <p className="summary__article">Summary</p>
+            <div className="summary__block">
+                <p>Items count:</p>
+                <p>Total price:</p>
+                <button id="make-order" type="submit" onClick={() => alert('modal')}>
+                    Make an Order
+                </button>
+            </div>
+        </div>
     </Container>
 );
 
