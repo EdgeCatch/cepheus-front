4a81a334e1bce7985db03b2abc6a15bed9d32427
Rexvel <teteriv22@gmail.com>
2020-05-20 17:49:06 +0300

    another one fix

--- src/view/components/Button/Button.js
+++ src/view/components/Button/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-
 import './button.scss';
 
 const Button = ({ children, onClick, className, disabled, active, ...attrs }) => {
