5aa373c4fc3c429093fad2f0e5e4bcccf3132c61
Alexandr Zosimchuk <al.zosimchuk@gmail.com>
2020-06-24 18:45:15 +0300

    update contract address

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const MARKET_ADDRESS = 'KT1LQpNebqh9tvr9NQ5oXDAhNZi8ahMVQPR5o';
+export const MARKET_ADDRESS = 'KT1LQpNebqh9tvr9NQ5oXDAhNZi8ahMVQPR5';
 export const TOKEN_ADDRESS = 'KT1RyYnrEcHwWnmjGddrnkMswwNBCo51kP6S';
