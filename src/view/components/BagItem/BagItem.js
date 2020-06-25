200767724d0ae922c74d2c269d099e611566a229
Rexvel <teteriv22@gmail.com>
2020-06-25 13:15:19 +0300

    style fixes

--- src/view/components/BagItem/BagItem.js
+++ src/view/components/BagItem/BagItem.js
@@ -7,63 +7,60 @@ import './bagItem.scss';
 import store from '../../../store/index';
 
 type Props = {
-  title: String,
-  price: String,
-  addToCart: Function
+    title: String,
+    price: String,
+    addToCart: Function,
 };
 const GoodsTileStyle = styled.div`
-  margin: 15px;
-  padding: 0;
-  text-align: center;
-  background-blend-mode: soft-light, normal;
-  display: flex;
-  flex-direction: column;
+    height: 400px;
+    margin: 15px;
+    padding: 0;
+    text-align: center;
+    background-blend-mode: soft-light, normal;
+    display: flex;
+    flex-direction: column;
 `;
 
 const CardWrapper = styled(Card)`
-  max-width: 250px;
-  border: none;
-  max-height: 380px;
+    border-radius: 10px 10px 0 0;
+    height: 85%;
+    max-width: 250px;
+    border: none;
 `;
 
 const CardBodyWrapper = styled(Card.Body)`
-  padding: 0;
+    padding: 0;
 `;
 
 const BagItem = (bag: Props) => {
-  const {
-    value: {
-      title,
-      price,
-      addToCart,
-      images: [firstImage]
+    const {
+        value: {
+            title,
+            price,
+            addToCart,
+            images: [firstImage],
+        },
+    } = bag;
+
+    function handleAddToCart() {
+        store.dispatch({ type: 'ADD_TO_CART', payload: bag.cid });
     }
-  } = bag;
 
-  function handleAddToCart() {
-    store.dispatch({ type: 'ADD_TO_CART', payload: bag.cid });
-  }
-
-  return (
-    <GoodsTileStyle>
-      <CardWrapper>
-        <CardBodyWrapper className="item__body">
-          <Card.Img
-            style={{ margin: '10px auto' }}
-            variant="top"
-            src={firstImage}
-          />
-          <img src="" alt="" />
-          <p>{title}</p>
-          <StarRating />
-          <p>${price}</p>
-        </CardBodyWrapper>
-      </CardWrapper>
-      <div className="add-to-card_btn purple" onClick={() => handleAddToCart()}>
-        <p> Add to cart</p>
-      </div>
-    </GoodsTileStyle>
-  );
+    return (
+        <GoodsTileStyle>
+            <CardWrapper>
+                <CardBodyWrapper className="item__body">
+                    <Card.Img style={{ margin: '10px auto', height: '70%' }} variant="top" src={firstImage} />
+                    <p>{title}</p>
+                    <StarRating />
+                    <p>${price}</p>
+                </CardBodyWrapper>
+            </CardWrapper>
+            <div className="add-to-card_btn purple" onClick={() => handleAddToCart()}>
+                <p> Add to cart</p>
+            </div>
+        </GoodsTileStyle>
+    );
 };
 
 export default BagItem;
